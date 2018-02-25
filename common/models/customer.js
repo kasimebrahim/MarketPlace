e33050ce5fd74287e7bde976dbb883ef9f654d33
kasim <se.kasim.ebrahim@gmail.com>
2018-02-25 13:24:13 +0300

    add edit customer && correct acl to Cus model

--- common/models/customer.js
+++ common/models/customer.js
@@ -1,5 +1,24 @@
 'use strict';
 
 module.exports = function(Customer) {
+    Customer.edit = function(cus, cb){
+        Customer.upsert(cus, function(err, models){
+            if(err){
+                return cb(err, "failed");
+            }
 
+            cb(null, models);
+        });
+    }
+    Customer.remoteMethod("edit", {
+        accepts: [{arg: 'cus', type: 'Object'}],
+        returns: {arg: 'message', type: 'array'},
+        http: {path:'/edit', verb: 'post'}
+    });
+
+    Customer.beforeRemote("edit", function(ctx, instance, next){
+        var accountId = ctx.req.accessToken.userId;
+        ctx.args.cus["accountId"] = accountId;
+        next();
+    });
 };
