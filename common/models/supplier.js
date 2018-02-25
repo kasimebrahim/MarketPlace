6c12a9e4523f42f688121b3a7bd1fcbd27ec1f67
kasim <se.kasim.ebrahim@gmail.com>
2018-02-25 13:46:01 +0300

    add edit to Supp && create acl

--- common/models/supplier.js
+++ common/models/supplier.js
@@ -1,5 +1,24 @@
 'use strict';
 
 module.exports = function(Supplier) {
+    Supplier.edit = function(supplier, cb){
+        Supplier.upsert(supplier, function(err, models){
+            if(err){
+                return cb(err, "failed");
+            }
 
+            cb(null, models);
+        });
+    }
+    Supplier.remoteMethod("edit", {
+        accepts: [{arg: 'supplier', type: 'Object'}],
+        returns: {arg: 'message', type: 'array'},
+        http: {path:'/edit', verb: 'post'}
+    });
+
+    Supplier.beforeRemote("edit", function(ctx, instance, next){
+        var accountId = ctx.req.accessToken.userId;
+        ctx.args.supplier["accountId"] = accountId;
+        next();
+    });
 };
