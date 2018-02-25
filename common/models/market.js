94df8d0af4d09f93bb70caaa789239b5d12070d1
kasim <se.kasim.ebrahim@gmail.com>
2018-02-25 14:52:40 +0300

    add Market && Market acl

--- common/models/market.js
+++ common/models/market.js
@@ -1,5 +1,29 @@
 'use strict';
 
 module.exports = function(Market) {
+    Market.edit = function(model, cb){
+        Market.upsert(model, function(err, market){
+            if(err){
+                return cb(err, "failed");
+            }
+            return cb(null, market);
+        })
+    }
 
+    Market.remoteMethod("edit", {
+        accepts:{arg:"model", type:"Object"},
+        returns:{arg:"market", type:"Object"},
+        http:{path:"/edit", verb:"post"}
+    });
+
+    Market.beforeRemote("edit", function(ctx, instance, next){
+        var Supplier = require("../../server/server.js").models.Supplier;
+        Supplier.findById(ctx.req.accessToken.userId, function(err, supplier){
+            if(err){
+                return next(err);
+            }
+            ctx.args.model.supplierId = supplier.id;
+            next();
+        });
+    });
 };
